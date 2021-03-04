a6c22f67ec490106366b56c839d29d91df43a3c0
Grzegorz Kupczyk <kupczykgrzeg@gmail.com>
2021-03-04 21:42:11 +0100

    Cover and NICE join button

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,24 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;400;700&display=swap');
+
+*,body,html{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body{
+  background: rgb(63,94,251);
+  background: radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(70,169,252,1) 100%);
+  font-size: 16px;
+  font-family: 'Noto Sans JP', sans-serif;
+  color: white;
+}
+
+a,a:visited,a:active{
+  color: white;
+  text-decoration: none;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,127 @@
 <template>
-  <div class="home">
-    <h1>Hello</h1>
+  <div class="container">
+    <div class="main">
+      <div class="cover">
+        <img src="@/assets/cover.png" alt="" />
+      </div>
+      <div class="text">
+        <p><a href="#" class="join">Join</a><br />the release party of the</p>
+        <h2><span class="nowrap">Software Architecture with C++</span></h2>
+      </div>
+    </div>
+    <div class="counter">00:00:00</div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Home',
-}
+  name: "Home",
+};
 </script>
+<style lang="scss" scoped>
+$colors: (
+  "first": #ffb43b,
+  "second": #f5711c,
+  "black": #141414,
+);
+
+.container {
+  margin: auto;
+  width: 70%;
+}
+
+.main {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  width: 100%;
+  margin-top: 3em;
+  padding: 2em;
+}
+
+.cover {
+  max-width: 30%;
+  perspective: 900px;
+  margin-right: 2em;
+
+  img {
+    max-width: 100%;
+    transform: scale3d(1, 1, 1) rotateY(-10deg);
+    box-shadow: 15px 20px 35px #005;
+  }
+}
+
+.text {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 1.7em;
+  font-size: 2em;
+  background-color: #3d3d3d;
+  text-align: center;
+  z-index: 1;
+
+  h2 {
+    color: #f5711c;
+    font-size: 1.2em;
+
+    small {
+      color: white;
+    }
+  }
+
+  .nowrap {
+    white-space: nowrap;
+  }
+}
+
+.join {
+  display: inline-block;
+  overflow: hidden;
+  position: relative;
+  vertical-align: bottom;
+  transition: all 0.5s;
+  font-weight: bold;
+  padding: 0.5em 1em;
+
+  &:after {
+    background: rgba(0, 0, 0, 0) center 1.98em / 100% 8px no-repeat;
+    background-image: linear-gradient(
+      45deg,
+      rgba(map_get($colors, "second"), 1) 15%,
+      rgba(map_get($colors, "first"), 1) 35%,
+      rgba(map_get($colors, "second"), 1) 85%
+    );
+
+    animation: underline-gradient 6s linear alternate infinite;
+    transition: all 0.5s;
+
+    content: "";
+    height: 100%;
+    left: 0;
+    position: absolute;
+    top: 5px;
+    width: 400%;
+    will-change: transform;
+    z-index: -1;
+  }
+
+  &:hover {
+    color: #3d3d3d;
+    &:after {
+      background-size: 100% 100%;
+      background-position-y: center;
+    }
+  }
+}
+
+@keyframes underline-gradient {
+  0% {
+    transform: translate3d(0%, 0%, 0);
+  }
+  100% {
+    transform: translate3d(-75%, 0%, 0);
+  }
+}
+</style>
